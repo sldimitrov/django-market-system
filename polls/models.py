5c584626d759b92d92121e411819b7c036b2aa79
Slavi FE <slavidimitrov54@gmail.com>
2025-12-06 22:41:29 +0200

    feat(django-app) add meaningful views, introduce templates

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+    DoesNotExist = None
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
